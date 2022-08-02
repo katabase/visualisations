b21e5f4e7d95abf4ab8779617090329e9935e114
paul k <paul.kervegan@hotmail.fr>
2022-08-02 17:29:36 +0200

    02.08.22 - smol update + readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# VISUALISATIONS OF THE DATA IN THE KATABASE PROJECT
+
+## Presentation
+A bunch of scripts / tools for the visualisation of data available at the end of 
+step 4 `TaggedData`. Some of the visualisations were originally created for the 
+website, others were made just to make sense of the data. Finally, a few scripts
+and visualisations were made for a [master's thesis](https://github.com/paulhectork/tnah2022_memoire).
+
+## Directory structure
+- `in/` : input directory (`json` and `xml` files)
+- `out/` : output directory (png figures and a json file)
+- `utils/` : useful tools to build the scripts: data preparation and such
+- all `*.py` scripts at root are used to make scripts.
+
+## Credits
+All scripts developped by Paul Kervegan in april-september 2022, except for
+`utils/reconciliator.py` and `utils/conversion_tables.py`, developped by A. Bartz. 
+Data used was created by different members of the Katabase project. 
+Available under GNU GPL v3.
+
+have fun !
--- teinametypes.py
+++ teinametypes.py
@@ -6,7 +6,8 @@ import glob
 import json
 import re
 
-from utils.matching import dpts, provinces, colonies, countries, events
+from utils.matching import dpts, provinces, colonies, countries, events, status
+from utils.rgx import namebuild
 
 
 # ---------------------------------------------------------------------------------------
@@ -36,6 +37,9 @@ def getnametype():
     "geo": [],
     "events" : []
   }  # to print the tei rpr of the special (aka, non-person tei-names)
+  fakes = 0  # to count the number of false positives in geo matching
+  #           (that is, the number of entries matched as geographic stuff
+  #           although they are a person's name
   for f in files:
     with open(f, mode="r") as fh:
       tree = etree.parse(fh, parser=parser)
@@ -43,20 +47,33 @@ def getnametype():
       for n in names:
 	# if there is text
         if n.text:
+
+          # fun part here: extract first names from parenthesis
+          parenth = re.search(r"\(.+\)?", n.text)  # check if there's text inside the parenthesis
+          if parenth is not None:
+            inp = re.sub(r"\(|\)", "", parenth[0])  # text in parenthesis
+            firstnm, matchstr, rebuilt = namebuild(inp)  # try to extract the full name
+          else:
+            inp = ""
+            matchstr = ""  # so that the case 4 condition doesn't fail
+
+          # begin classifying the names
           t = n.text.lower()
           if re.search(r"^(le|la)\sm[êe]me\.?$", t) \
               or re.search(r"^(documents?|(divers)|\s)+$", t) \
               or re.search(r"chartes?", t):
             out["divers"] += 1
-          elif any(d in t for d in dpts)\
-              or any(p in t for p in provinces)\
-              or any(c in t for c in colonies)\
-              or any(c in t for c in countries.keys()):
+          elif any(p in re.sub(r"(\.|,|(\s-)|(-\s))+", " ", t).split() for p in provinces) \
+              or any(d in re.sub(r"(\.|,|(\s-)|(-\s))+", " ", t).split() for d in dpts) \
+              or any(c in re.sub(r"(\.|,|(\s-)|(-\s))+", " ", t).split() for c in colonies) \
+              or any(c in re.sub(r"(\.|,|(\s-)|(-\s))+", " ", t).split() for c in countries.keys()):
             out["geo"] += 1
             snowflakes["geo"].append(str(etree.tostring(
                                       n, xml_declaration=False, encoding="utf-8").decode("utf-8")
             ))
-          elif any(e in t for e in events.keys()):
+            if matchstr == "" and not any(s in t for s in status):
+              fakes += 1
+          elif any(e in re.sub(r"(\.|,|(\s-)|(-\s))+", " ", t).split() for e in events):
             out["events"] += 1
             snowflakes["events"].append(str(etree.tostring(
                                       n, xml_declaration=False, encoding="utf-8").decode("utf-8")
@@ -69,7 +86,7 @@ def getnametype():
   # write the dict to file
   with open("out/teinametypes.json", mode="w") as fh:
     json.dump(snowflakes, fh, indent=4)
-
+  print(fakes)
   return out
 
 
